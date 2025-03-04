abdc9a803f9c79d9e56cf129aa5c6af8052e1bce
Artyom <sadmerrycom2@gmail.com>
2025-03-04 14:46:04 +0300

    fixed lack of self params in analyzer.py

--- dashboard/logic/analyzer.py
+++ dashboard/logic/analyzer.py
@@ -2,14 +2,14 @@ class Analyzer:
     def load(self, f: str):
         self.data = [0, 1, 2, 3, 4, 5]
     
-    def context_recall():
+    def context_recall(self):
         return 0.57
     
-    def context_precision():
+    def context_precision(self):
         return 0.43
     
-    def answer_correctness_literal():
+    def answer_correctness_literal(self):
         return 0.23
     
-    def answer_correctness_neural():
+    def answer_correctness_neural(self):
         return 0.1

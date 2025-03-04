0e459a05cf7166245f1f3ef55deba3a7a79dff40
Artyom <sadmerrycom2@gmail.com>
2025-03-04 14:28:31 +0300

    analyzer mock

---
+++ dashboard/logic/analyzer.py
@@ -0,0 +1,15 @@
+class Analyzer:
+    def load(self, f: str):
+        self.data = [0, 1, 2, 3, 4, 5]
+    
+    def context_recall():
+        return 0.57
+    
+    def context_precision():
+        return 0.43
+    
+    def answer_correctness_literal():
+        return 0.23
+    
+    def answer_correctness_neural():
+        return 0.1
